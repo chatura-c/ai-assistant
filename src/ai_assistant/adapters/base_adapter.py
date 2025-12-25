259a7b45ab615775572e6016ef39e33471eb92e8
chaturajd <chaturajd@gmail.com>
2025-12-25 12:52:30 +0530

    added hyprland adapter

---
+++ src/ai_assistant/adapters/base_adapter.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class ContextFrame:
+    text: str | None
+    app: str | None
+    is_ignored: bool
+
+class BaseAdapter(ABC):
+    @abstractmethod
+    def replace_selected_text(self, new_text):
+        pass
+
+    @abstractmethod
+    def get_mouse_pos(self) -> tuple[int, int]:
+        pass
+
+    @abstractmethod
+    def get_context_frame(self) -> ContextFrame:
+        pass
+
+    @abstractmethod
+    def copy_text_into_clipboard(self, text):
+        pass
+
---
+++ src/ai_assistant/adapters/hyprland.py
@@ -0,0 +1,83 @@
+
+import subprocess
+from ai_assistant.adapters.base_adapter import BaseAdapter, ContextFrame
+import json
+from collections import deque
+
+class HyprlandAdapter(BaseAdapter):
+    def __init__(self, app_name: str, max_history=10) -> None:
+        super().__init__()
+        self.app_name = app_name
+        self.ignore_classes = [app_name]
+        
+        self.stack: deque[ContextFrame] = deque(maxlen=max_history)
+        self.stack.append(ContextFrame(None, None, False))
+
+    def replace_selected_text(self, new_text):
+        pass
+
+    def get_focused_window(self) -> str | None:
+        try:
+            output = subprocess.check_output(["hyprctl", "activewindow", "-j"], timeout=0.1)
+            data = json.loads(output)
+            
+            return data.get("class") 
+        except:
+            return None
+    
+    def get_mouse_pos(self) -> tuple[int, int]:
+        pos = subprocess.check_output(["hyprctl", "cursorpos"]).decode().strip()
+        x, y = map(int, pos.split(","))
+        return (x, y)
+
+    def get_context_frame(self) -> ContextFrame:
+        current_focus = self.get_focused_window()
+        raw_text = self._get_raw_wl_paste()
+        
+        is_ignored = current_focus in self.ignore_classes
+        last_frame = self.stack[-1]
+
+        if raw_text and raw_text != last_frame.text:
+            if is_ignored:
+                raw_text = None
+
+            new_frame = ContextFrame(
+                text=raw_text,
+                app=current_focus,
+                is_ignored=is_ignored
+            )
+            self.stack.append(new_frame)
+
+        # if current_focus != last_frame.app or (raw_text and raw_text != last_frame.text):
+        #     new_frame = ContextFrame(
+        #         text=raw_text if not is_ignored else "",
+        #         app=current_focus,
+        #         is_ignored=is_ignored
+        #     )
+        #     self.stack.append(new_frame)
+
+        for frame in reversed(self.stack):
+            if not frame.is_ignored and frame.text:
+                return frame
+
+        return ContextFrame(None, None, False)
+    
+    def _get_raw_wl_paste(self) -> str | None:
+        """Internal helper to grab text without logic."""
+        try:
+            primary = subprocess.check_output(
+                ["wl-paste", "--primary", "--no-newline"], 
+                stderr=subprocess.DEVNULL, timeout=0.05
+            ).decode().strip()
+            
+            if primary: return primary
+
+            return subprocess.check_output(
+                ["wl-paste", "--no-newline"], 
+                stderr=subprocess.DEVNULL, timeout=0.05
+            ).decode().strip()
+        except:
+            return None
+
+    def copy_text_into_clipboard(self, text):
+        subprocess.run(["wl-copy"], input=text)
