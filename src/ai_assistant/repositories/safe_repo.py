5d81c5575c229ae2020fea6df6df0d06845d7ad3
chaturajd <chaturajd@gmail.com>
2025-12-28 06:16:31 +0530

    added safe repo using keyring

---
+++ src/ai_assistant/repositories/safe_repo.py
@@ -0,0 +1,32 @@
+from ai_assistant.core.repository import Repository
+from ai_assistant.core.safekey import SafeKey
+import keyring
+from typing import Optional, List
+
+class SafeKeyRepository(Repository[SafeKey]):
+    def __init__(self, service_name: str):
+        self.service_name = service_name
+
+    def create(self, entity: SafeKey) -> SafeKey:
+        keyring.set_password(self.service_name, entity.key, entity.value)
+        return entity
+
+    def get(self, id: str) -> Optional[SafeKey]:
+        value = keyring.get_password(self.service_name, id)
+        if value is None:
+            return None
+        return SafeKey(key=id, value=value)
+
+    def update(self, id: str, entity: SafeKey) -> None:
+        if id != entity.key:
+            self.remove(id)
+        keyring.set_password(self.service_name, entity.key, entity.value)
+
+    def remove(self, id: str) -> None:
+        try:
+            keyring.delete_password(self.service_name, id)
+        except keyring.errors.PasswordDeleteError:
+            pass
+
+    def get_all(self) -> List[SafeKey]:
+        raise NotImplementedError("Cannot list all keys.")
